4d5df97d9905b707099dba6b01a2a3a0ca4901f6
AronssonFredrik <fredrik.e.aronsson@gmail.com>
2022-02-16 19:56:57 +0100

    Update name of main package

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-httpTransportState'),
+      dir: require('path').join(__dirname, './coverage/http-response-state'),
       subdir: '.',
       reporters: [
         { type: 'html' },
