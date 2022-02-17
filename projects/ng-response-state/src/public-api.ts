50a87438f2442f46fbf1d9e7c192c2aa11ab273c
AronssonFredrik <fredrik.e.aronsson@gmail.com>
2022-02-17 18:59:16 +0100

    Correcting the naming

---
+++ projects/ng-response-state/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ng-response-state
+ */
+
+export * from './lib/service-response-state.service';
--- projects/ngrx-http-response-state/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ngrx-http-response-state
- */
-
-export * from './lib/http-response-state.service';
