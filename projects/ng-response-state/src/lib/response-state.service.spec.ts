372d3379d8e4f4caf063d7da1436f2ffd78e7c6d
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2022-03-03 18:34:52 +0100

    feat(response state): Initial service handler
    Resolves #4

--- projects/ng-response-state/src/lib/response-state.service.spec.ts
+++ projects/ng-response-state/src/lib/response-state.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
+import { ResponseState, ResponseStateType } from './response-state.interface';
 
 import { ResponseStateService } from './response-state.service';
 
-describe('NgServiceResponseStateService', () => {
+describe('NgResponseStateService', () => {
   let service: ResponseStateService;
+  let responseState: ResponseState;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ResponseStateService);
+    await service.responseState$.subscribe((type) => responseState = type);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be created', () => expect(service).toBeTruthy());
+
+  it('should set state to none', () => expect(responseState.state).toBe(ResponseStateType.none));
+  it('should NOT be loading', () => expect(responseState.isLoading).toBe(false));
+  it('should NOT be error', () => expect(responseState.isError).toBe(false));
+  it('should NOT be successfull', () => expect(responseState.isSuccess).toBe(false));
+
+  describe("Response is loading", () => {
+    beforeEach(() => service.setLoading());
+
+    it('should set state to loading', () => expect(responseState.state).toBe(ResponseStateType.loading));
+    it('should be loading', () => expect(responseState.isLoading).toBe(true));
+    it('should NOT be error', () => expect(responseState.isError).toBe(false));
+    it('should NOT be successfull', () => expect(responseState.isSuccess).toBe(false));
   });
+
+  describe("Response is succesfull", () => {
+    beforeEach(() => service.setSuccess());
+
+    it('should set state to successfull', () => expect(responseState.state).toBe(ResponseStateType.success));
+    it('should be loading', () => expect(responseState.isLoading).toBe(false));
+    it('should NOT be error', () => expect(responseState.isError).toBe(false));
+    it('should NOT be successfull', () => expect(responseState.isSuccess).toBe(true));
+  });
+
+  describe("Response is error", () => {
+    beforeEach(() => service.setError());
+
+    it('should set state to loading', () => expect(responseState.state).toBe(ResponseStateType.error));
+    it('should be loading', () => expect(responseState.isLoading).toBe(false));
+    it('should NOT be error', () => expect(responseState.isError).toBe(true));
+    it('should NOT be successfull', () => expect(responseState.isSuccess).toBe(false));
+  });
+
 });
